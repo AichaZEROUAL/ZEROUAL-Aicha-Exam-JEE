06f6115b191af070e90b7d8f5803beeaa8546083
Aicha ZEROUAL <aicha.zeroual01@gmail.com>
2026-05-11 09:05:01 +0100

    backend start

---
+++ backend/src/test/java/org/aichazeroual/backendaichazeroual/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aichazeroual.backendaichazeroual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
